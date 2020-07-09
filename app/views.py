ac0cff580cb01f61a5913acabaf6672bfaed83ff
TBone-R <trivolet@gmail.com>
2020-07-09 17:44:29 +0200

    Dynamic jscript, use of CDN for boostrap and jquery, use of css for better looking interface and use of multi html %block% project structure for future upgrade.

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import Flask, request,  render_template
+from flask import Flask, request, make_response,  render_template, url_for, jsonify
 from flask_wtf import FlaskForm
 from wtforms import StringField
 from wtforms.validators import DataRequired
@@ -7,6 +7,7 @@ app = Flask(__name__)
 
 app.config.from_object('config')
 
+
 class UserInput(FlaskForm):
     user_question = StringField('', validators=[DataRequired()])
 
@@ -14,10 +15,18 @@ class UserInput(FlaskForm):
 @app.route('/')
 @app.route('/index/')
 def index():
-    return render_template('index.html', form=UserInput())
+    return render_template('question.html', form=UserInput())
 
-@app.route('/', methods=['POST'])
-@app.route('/index/', methods=['POST'])
+
+@app.route('/answer', methods=['POST'])
 def answer():
-    question = request.form['user_question']
-    return question
+
+    question = request.get_json()['user_input']
+    # bot_answer = {"location":models.question.get_parsant, "bot_answer": models.question.answer}
+    bot_answer = {"location": question, "bot_answer": question}
+    text = open('app/templates/answer', 'r').read().encode('latin1').decode('utf8')
+
+    string_answer = text.format(location=bot_answer['location'], bot_answer=bot_answer['bot_answer'])
+
+    res = make_response(jsonify({'answer': string_answer}), 200)
+    return res
