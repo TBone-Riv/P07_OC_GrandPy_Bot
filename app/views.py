cdbedcacf8388c26c15540e362355871931186b6
Thibaut Rivolet <trivolet@gmail.com>
2020-07-16 14:26:17 +0200

    use of ajax and templates twix

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, make_response,  render_template, url_for, jsonify
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 app = Flask(__name__)
@@ -9,7 +9,9 @@ app.config.from_object('config')
 
 
 class UserInput(FlaskForm):
-    user_question = StringField('', validators=[DataRequired()])
+    user_question = StringField('')
+
+    submit = SubmitField('Submit')
 
 
 @app.route('/')
@@ -18,15 +20,10 @@ def index():
     return render_template('question.html', form=UserInput())
 
 
-@app.route('/answer', methods=['POST'])
+@app.route('/answer', methods=['POST', 'GET'])
 def answer():
-
-    question = request.get_json()['user_input']
+    question = request.json['user_input']
     # bot_answer = {"location":models.question.get_parsant, "bot_answer": models.question.answer}
     bot_answer = {"location": question, "bot_answer": question}
-    text = open('app/templates/answer', 'r').read().encode('latin1').decode('utf8')
-
-    string_answer = text.format(location=bot_answer['location'], bot_answer=bot_answer['bot_answer'])
-
-    res = make_response(jsonify({'answer': string_answer}), 200)
+    res = make_response(jsonify(bot_answer), 200)
     return res
